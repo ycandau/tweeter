8ca828f7e28cf948dfbadaa2552a7d9ef286b427
Yves Candau <ycandau@gmail.com>
2021-07-12 07:40:46 +0000

    Made small changes to understand how the initial codebase works

--- server/lib/date-adjust.js
+++ server/lib/date-adjust.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 
 // Require fs to write to initial-tweets.json
-const fs = require("fs");
+const fs = require('fs');
 // Requiring a JSON file automatically parses it and returns the data.
 let tweetsJSON = require('../data-files/initial-tweets.json');
 
@@ -10,13 +10,17 @@ let tweetsJSON = require('../data-files/initial-tweets.json');
 module.exports = () => {
   // One day in milliseconds is 86400000ms or...
   // oneDayMs = 1000 milliseconds * 60 seconds * 60 minutes * 24 hours.
-  const oneDayMs = 1000 * 60 * 60 * 24
+  const oneDayMs = 1000 * 60 * 60 * 24;
   // Subtract one day in milliseconds (oneDayMs) times the tweets length minus the current index.
   // This keeps the newest tweets at the bottom, and allows for further tweets to be added.
   tweetsJSON = tweetsJSON.map((tweet, index) => {
-    tweet.created_at = Date.now() - (oneDayMs * (tweetsJSON.length - index));
+    tweet.created_at = Date.now() - oneDayMs * (tweetsJSON.length - index);
     return tweet;
   });
   // Re-write the tweets with the new date values.
-  fs.writeFileSync('server/data-files/initial-tweets.json', JSON.stringify(tweetsJSON, null, 2), { encoding: "utf8" });
+  fs.writeFileSync(
+    'server/data-files/initial-tweets.json',
+    JSON.stringify(tweetsJSON, null, 2),
+    { encoding: 'utf8' }
+  );
 };
