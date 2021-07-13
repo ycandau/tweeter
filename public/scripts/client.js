1828185acb3b9358382321cd8e73975f57dc0acd
Yves Candau <ycandau@gmail.com>
2021-07-13 20:08:01 +0000

    Implemented function to create a tweet DOM element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,13 +1,39 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// client.js
 
-$(document).ready(function () {
-  $('.tweet')
-    .children('footer')
-    .children()
-    .first()
-    .text(timeago.format(1473245023718));
-});
+const createTweetElement = (tweet) => {
+  return $(`
+    <article class="tweet">
+      <header>
+        <img src="${tweet.user.avatars}" />
+        <div>${tweet.user.name}</div>
+        <div>${tweet.user.handle}</div>
+      </header>
+      <div>${tweet.content.text}</div>
+      <footer>
+        <div>${timeago.format(tweet.created_at)}</div>
+        <div>
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>`);
+};
+
+//------------------------------------------------------------------------------
+// Testing
+
+const aTweet = {
+  user: {
+    name: 'Newton',
+    avatars: 'https://i.imgur.com/73hZDYK.png',
+    handle: '@SirIsaac',
+  },
+  content: {
+    text: 'If I have seen further it is by standing on the shoulders of giants',
+  },
+  created_at: 1461116232227,
+};
+
+const $tweet = createTweetElement(aTweet);
+$('#tweets-container').append($tweet);
