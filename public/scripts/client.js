1315cfe9cc0144ba18debc30103ecd75b14cefd9
Yves Candau <ycandau@gmail.com>
2021-07-13 04:07:04 +0000

    Added time passed calculation using timeago.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,3 +3,11 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+$(document).ready(function () {
+  $('.tweet')
+    .children('footer')
+    .children()
+    .first()
+    .text(timeago.format(1473245023718));
+});
